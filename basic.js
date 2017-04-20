4d59942170dd1dd6b99421f37c377bb7e5d857a6
meeshyd <michellemdidier@gmail.com>
2017-04-20 16:33:58 -0500

    added save method to both basic and cloze js files

--- basic.js
+++ basic.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 function BasicCard(front,back) {
 	if (!(this instanceof BasicCard)) { 
     return new BasicCard(front,back);
@@ -5,6 +7,19 @@ function BasicCard(front,back) {
 
 	this.front = front;
 	this.back = back;
+};
+
+//this method will store flashcard into a flashcards.txt file
+BasicCard.prototype.saveFlashcard = function(){
+	//storing text to save in a variable
+	let logText = "Front: " + this.front + "\n" + "Back: " + this.back + "\n\n\n";
+	//function to append logText to flashcards.txt file
+	fs.appendFile("flashcards.txt", logText, "utf8", function(error) {
+		// if there is an error, log the error
+		if (error) {
+			console.log(error);
+		};
+	});
 };
 
 module.exports = BasicCard;
--- cloze.js
+++ cloze.js
@@ -5,32 +5,34 @@ function ClozeCard (text,cloze){
 	if (!(this instanceof ClozeCard)) { 
     return new ClozeCard(text,cloze);
   	};
-  	//throw or log an error when the cloze deletion does not appear in the input text.
+
 	this.text = text;
 	this.cloze = cloze;
 	this.partial = this.text.replace(this.cloze,'...');
 
-	//if cloze not found in full text, log error message
-	if (this.text.indexOf(this.cloze)=== -1) {
-		console.log('Error: Cloze not found in text!');
-	}else{
-	
-		//store text to be appended to flashcards.txt in a variable
-		let logText = "Partial text: " + this.partial + "\n" + "Cloze: " + this.cloze + "\n\n\n";
-		//function to append logText to flashcards.txt file
-		fs.appendFile("flashcards.txt", logText, "utf8", function(error) {
-	    // if there is an error, log the error
-		    if (error) {
-		        console.log(error);
-		    };
-		});
+	//if cloze not found in full text, log error message.
+	if (this.text.indexOf(this.cloze) === -1) {
+		console.log('Invalid Cloze Error: '+this.cloze+' '+'was not found in '+this.text);
 	};
-	//deleting this.text from object so that answer does not display unless answer function is called
+	//removing this.text from object. full text can only be displayed by calling fullText method.
 	delete this.text;
 };
 
-//function to display full text
-ClozeCard.prototype.answer = function(){
+//this method will store flashcard into a flashcards.txt file
+ClozeCard.prototype.saveFlashcard = function(){
+	//storing text to save in a variable
+	let logText = "Partial text: " + this.partial + "\n" + "Cloze: " + this.cloze + "\n\n\n";
+	//function to append logText to flashcards.txt file
+	fs.appendFile("flashcards.txt", logText, "utf8", function(error) {
+		// if there is an error, log the error
+		if (error) {
+			console.log(error);
+		};
+	});
+};
+
+//this method displays the full text
+ClozeCard.prototype.fullText = function(){
 	console.log(this.cloze + this.partial.replace('...',''))
 };
 
